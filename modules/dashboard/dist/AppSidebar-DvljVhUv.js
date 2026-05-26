8c1d6785a519b1acbf9222956a9bc90049d61e99
RaghuwanshiAditya <ga.1mn.io@gmail.com>
2026-05-26 15:03:26 +0530

    May,26
    May,26

--- modules/dashboard/dist/AppSidebar-DvljVhUv.js
+++
@@ -1,4 +0,0 @@
-import { _ as f } from "./AppSidebar.vue_vue_type_script_setup_true_lang-7vsr2ejI.js";
-export {
-  f as default
-};
---
+++ modules/dashboard/dist/index-GU1HVl_k.js
@@ -0,0 +1,40 @@
+import { H as r } from "./main--b4Xr_Kp.js";
+import { a9 as t, G as d, a2 as s, H as l, al as i } from "./runtime-dom.esm-bundler-7m8Vyt41.js";
+var u = {
+  name: "Portal",
+  props: {
+    appendTo: {
+      type: [String, Object],
+      default: "body"
+    },
+    disabled: {
+      type: Boolean,
+      default: !1
+    }
+  },
+  data: function() {
+    return {
+      mounted: !1
+    };
+  },
+  mounted: function() {
+    this.mounted = r();
+  },
+  computed: {
+    inline: function() {
+      return this.disabled || this.appendTo === "self";
+    }
+  }
+};
+function p(e, f, o, m, n, a) {
+  return a.inline ? t(e.$slots, "default", {
+    key: 0
+  }) : n.mounted ? (l(), d(i, {
+    key: 1,
+    to: o.appendTo
+  }, [t(e.$slots, "default")], 8, ["to"])) : s("", !0);
+}
+u.render = p;
+export {
+  u as s
+};
